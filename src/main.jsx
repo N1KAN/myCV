1f413cc86de050bc743f9ae11e3e31d160e0ff57
Nikan Seyed Marandy <nikan.seyedmarandy@stud.h-da.de>
2024-11-06 13:45:25 +0100

    Kontakt update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Kontakt from './komponenten/Kontakt.jsx';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/contact",
+      element: <Kontakt></Kontakt>,  // Hier könnte deine Kontakt-Komponente sein
+    },
+  ],
+  {
+    basename: "/myCV",  // Setzt den Basis-URL-Pfad für alle Routen
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
