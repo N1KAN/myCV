06a6ca2ff5ce824964a211e51eeee7f20988b6fa
Nikan Seyed Marandy <nikan.seyedmarandy@stud.h-da.de>
2024-11-06 14:00:38 +0100

    Body-commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './komponenten/Header'
+import Body from './komponenten/Body'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <Header></Header>
+    <Body></Body>
     </>
   )
 }
